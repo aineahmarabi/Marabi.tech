81de33d7ed6cfe4c1dc9bf3068312d26df773fab
aineahmarabi <aineahbwire@gmail.com>
2024-10-14 23:43:39 +0300

    Typo correction home page

--- src/app/components/email.tsx
+++ src/app/components/email.tsx
@@ -1,6 +1,6 @@
 'use client'
 export default function Email(){
     return(
-        <p className="cursor-pointer hover:text-[#3D9BE9] text-base lg:text-lg md:text-lg text-black"><a target="_blank" rel="noopener noreferrer" href="mailto:info@marabi.tech">info@zahavconsulting.com</a></p>
+        <p className="cursor-pointer hover:text-[#3D9BE9] text-base lg:text-lg md:text-lg text-black"><a target="_blank" rel="noopener noreferrer" href="mailto:info@marabi.tech">info@<br></br>zahavconsulting.com</a></p>
     );
 }
