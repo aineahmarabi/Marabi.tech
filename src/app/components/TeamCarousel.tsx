3e28a071ea6349dc89fea2e3f97b7ecdc00e2075
aineahmarabi <aineahbwire@gmail.com>
2024-10-14 21:45:13 +0300

    Made mobile view responsive

--- src/app/components/TeamCarousel.tsx
+++ src/app/components/TeamCarousel.tsx
@@ -22,8 +22,8 @@ const dummyTeamMembers: TeamMember[] = [
   {
     id: 1,
     name: "Eljones Odongo",
-    position: "Software Engineer - Backend",
-    description: "Passionate about creating efficient and scalable web applications.",
+    position: "Software Engineer - Full Stack",
+    description: "Master of both front-end finesse and back-end brilliance, I craft seamless digital experiences from start to finish. Code wizard by day, problem-solver always.",
     photo: "images/Eljones.jpg",
     socialMedia: {
       twitter: "https://x.com/eljones_ke",
@@ -35,7 +35,7 @@ const dummyTeamMembers: TeamMember[] = [
     id: 2,
     name: "Aineah Marabi",
     position: "UX/UI Developer",
-    description: "Crafting intuitive and beautiful user experiences.",
+    description: "The architect of intuitive, eye-catching designs. I turn user needs into sleek, seamless interfaces that are as functional as they are beautiful. Where creativity meets usability.",
     photo: "images/Inea.jpg",
     socialMedia: {
         twitter: "https://x.com/inea_marabi",
@@ -47,8 +47,8 @@ const dummyTeamMembers: TeamMember[] = [
   {
     id: 3,
     name: "Harry Rotich",
-    position: "Developer",
-    description: "Ensuring smooth project execution and team collaboration.",
+    position: "Zoho Developer",
+    description: "The maestro of Zoho solutions, crafting custom applications that streamline workflows and boost productivity. I turn complexity into clarity, making your business run like a well-oiled machine.",
     photo: "images/Harry.jpg",
     socialMedia: {
         linkedin: "https://www.linkedin.com/in/harry-rotich/",
@@ -59,12 +59,10 @@ const dummyTeamMembers: TeamMember[] = [
   {
     id: 4,
     name: "Charles Maina",
-    position: "Project Manager",
-    description: "Turning data into actionable insights.",
+    position: "Developer",
+    description: "Code ninja and tech enthusiast, I bring ideas to life through innovative solutions and seamless integrations. Problem-solver by nature, I turn challenges into code with flair.",
     photo: "images/Charles.jpg",
     socialMedia: {
-        twitter: "https://twitter.com/johndoe",
-        linkedin: "https://linkedin.com/in/johndoe",
         github: "https://github.com/mikewhis",
     },
   },
@@ -72,7 +70,7 @@ const dummyTeamMembers: TeamMember[] = [
     id: 5,
     name: "Levis Murunga",
     position: "Business Development Executive",
-    description: "Developing strategies to reach and engage our target audience.",
+    description: "The growth guru, forging connections and opening doors to new opportunities. I strategize, negotiate, and cultivate relationships that drive success and fuel innovation.",
     photo: "images/Levis.jpg",
     socialMedia: {
       linkedin: "https://www.linkedin.com/in/levis-murunga",
@@ -131,18 +129,18 @@ const TeamCarousel: React.FC = () => {
   };
 
   return (
-    <div className="lg:w-[90%] h-[80vh] overflow-hidden bg-transparent items-center">
+    <div className="relative w-full h-[80vh] overflow-hidden bg-transparent flex justify-center items-center mx-auto">
       {dummyTeamMembers.map((member, index) => (
         <div
           key={member.id}
-          className="absolute left-1/2 w-[300px] h-auto bg-white rounded-lg shadow-lg"
+          className="absolute lg:w-[300px] w-[200px] lg:h-auto h-[70vh] bg-white rounded-lg shadow-lg"
           style={getCardStyle(index)}
         >
-          <img src={member.photo} alt={member.name} className="w-full h-96 object-cover rounded-t-lg" />
-          <div className="p-4">
-            <h2 className="text-xl font-bold">{member.name}</h2>
-            <p className="text-sm text-gray-600">{member.position}</p>
-            <p className="mt-2 text-sm">{member.description}</p>
+          <img src={member.photo} alt={member.name} className="w-[200px] lg:w-full lg:h-96 h-[40%] object-cover rounded-t-lg" />
+          <div className="p-4 h-[50%]">
+            <h2 className="text-xl font-bold flex justify-center">{member.name}</h2>
+            <p className="text-sm text-[#3D9BE9] flex justify-center">{member.position}</p>
+            <p className="mt-2 text-sm text-start">{member.description}</p>
             <div className="mt-4 flex justify-center space-x-3">
               {member.socialMedia.twitter && <FaXTwitter className="text-black cursor-pointer" />}
               {member.socialMedia.linkedin && <FaLinkedin className="text-blue-700 cursor-pointer" />}
--- src/app/components/team.tsx
+++ src/app/components/team.tsx
@@ -6,6 +6,9 @@ export default function Team(){
             <p className="text-sm md:text-lg lg:text-lg pb-5 text-justify">Five oddballs walked into a startup. Sounds like the setup for a bad joke, right? But nah, it&apos;s just us – the   code-slinging trio, the deal-maker, and the chaos-tamer. We&apos;re the square pegs who ditched the round holes and built our own damn shape. Conventional? Hell no. Effective? You bet your last line of buggy code we are.
             </p>
             <TeamCarousel />
+            <p className="text-sm md:text-lg lg:text-lg pb-5 text-justify">
+            we&apos;re more than just a collection of techies—we&apos;re a crew of creative problem-solvers who thrive on turning challenges into opportunities. Each of us brings a unique spark, blending brains, creativity, and a little bit of magic to build solutions that don&apos;t just work but wow. We&apos;re the strategists who ask the tough questions, the developers who geek out over clean code, and the visionaries who won&apos;t settle for good enough.
+            </p>
         </div>
     );
 }
